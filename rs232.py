4ffe0e56ecc17a646460089a785fc8653bea25fe
nsobolewska <n_sobolewska@wp.pl>
2017-06-10 15:45:46 +0200

    Buttons definition

--- rs232.py
+++ rs232.py
@@ -15,7 +15,43 @@ class Window(QMainWindow):
         self.gridLayout = QGridLayout()
         self.okno.setLayout(self.mainLayout)
         self.setWindowTitle("Menu")
+        self.buttons()
         self.show()
+
+    def buttons(self):
+        self.btn0 = QPushButton('Nadajnik', self)
+        self.btn0.clicked.connect(self.on_click0)
+        self.btn0.resize(400,100)
+        self.btn0.move(50, 50)
+
+        self.btn1 = QPushButton('Odbiornik', self)
+        self.btn1.clicked.connect(self.on_click1)
+        self.btn1.resize(400, 100)
+        self.btn1.move(50, 170)
+
+        self.btn2 = QPushButton('Zakoncz', self)
+        self.btn2.clicked.connect(self.close)
+        self.btn2.resize(400, 100)
+        self.btn2.move(50, 290)
+
+    def on_click0(self):
+        print("to nadajnik")
+    def on_click1(self):
+        print("to odbiornik")
+    def close(self):
+        choice = QMessageBox.question(self, 'Extract', "Are you sure you want to quit?",
+                                      QMessageBox.Yes | QMessageBox.No)
+
+        if choice == QMessageBox.Yes:
+            print("Zamykamy sie")
+            sys.exit()
+        else:
+            pass
+
+    def closeEvent(self, QCloseEvent):
+        QCloseEvent.ignore()
+        self.close()
+
 
 def run():
     app = QApplication(sys.argv)
